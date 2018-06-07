fa30d291983b3f2c56fe68b7e1ad3d4fd42cd5d9
Imants Volkovs <imants.volkovs@gmail.com>
2018-06-07 20:12:32 +0300

    added service

--- src/Ratio.Api/Startup.cs
+++ src/Ratio.Api/Startup.cs
@@ -1,25 +1,36 @@
 ﻿namespace VolskSoft.Ratio.Api
 {
-    using System;
-    using System.IO;
     using Owin;
 
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Run(context =>
-            {
-                context.Response.ContentType = "text/plain";
-                return context.Response.WriteAsync("Hello, world.");
-            });
 
-            //var wwwroot = Path.Combine(
-            //    AppDomain.CurrentDomain.BaseDirectory,
-            //    "wwwroot");
-            //AutofacInitializer.Initialize();
-            //var configurator = Configure(wwwroot, AutofacInitializer.AutofacContainer);
-            //configurator(app);            
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+        
+            //  Enable attribute based routing
+            //  http://www.asp.net/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+
+
+            app.Map("", pipeline =>
+            {
+                pipeline.Use((context, next) =>
+                {
+                    context.Response.ContentType = "text/plain";
+                    return context.Response.WriteAsync("Hello, world.");
+                });
+            });     
         }
     }
 }
